3037f373a3ded91e06b6fcb179e98d73dec95329
whitetiger1002 <nike199000@outlook.com>
2022-10-08 08:28:48 +0600

    add try/except statement

--- api.py
+++ api.py
@@ -25,70 +25,84 @@ def get_data():
 	driver.get(target_link)
 	time.sleep(6)
 
-	driver.find_element(By.ID, 'cookies-agree-all').click()
-
+	# driver.find_element(By.ID, 'cookies-agree-all').click()
+	
 	product_title = driver.find_element(By.CLASS_NAME, 'pdp-title').text
 	product_price = driver.find_element(By.CLASS_NAME, 'pdp-prices').text
 
 	infos = driver.find_elements(By.CLASS_NAME, 'info')
 
-	title1 = infos[0].find_element(By.TAG_NAME, 'h3').text
-	contents1 = infos[0].find_elements(By.TAG_NAME, 'li')
+	title1, title2, title3, title4, sub_title = ['', '', '', '', '']
+	txt_contents1, txt_contents2, txt_contents3, txt_contents4 = ['', '', '', '']
+	err = ""
 
-	txt_contents1 = ""
-	for line in contents1:
-		txt_contents1 = txt_contents1 + line.text + " "
+	try:
+		for info in infos:
+			try:
+				match info.find_element(By.TAG_NAME, 'h3').text:
+					case "Información general":
+						title1 = info.find_element(By.TAG_NAME, 'h3').text
+						contents1 = info.find_elements(By.TAG_NAME, 'li')
+						
+						for line in contents1:
+							txt_contents1 = txt_contents1 + line.text + " "
 
-	title2 = infos[1].find_element(By.TAG_NAME, 'h3').text
-	contents2 = infos[1].find_elements(By.TAG_NAME, 'li')
+					case "Ingredientes y alérgenos":
+						title2 = info.find_element(By.TAG_NAME, 'h3').text
+						contents2 = info.find_elements(By.TAG_NAME, 'li')
+						
+						for line in contents2:
+							txt_contents2 = txt_contents2 + line.text + " "
 
-	txt_contents2 = ""
-	for line in contents2:
-		txt_contents2 = txt_contents2 + line.text + ""
+					case "Conservación y utilización":
+						title4 = info.find_element(By.TAG_NAME, 'h3').text
+						contents4 = info.find_elements(By.TAG_NAME, 'li')
+						
+						for line in contents4:
+							txt_contents4 = txt_contents4 + line.text + " "
 
-	# Nutrients
-	title3 = infos[2].find_element(By.TAG_NAME, 'h4').text
-	sub_title = infos[2].find_element(By.TAG_NAME, 'p').text
+			except:
+				try:
+					title3 = info.find_element(By.TAG_NAME, 'h4').text
+					sub_title = info.find_element(By.TAG_NAME, 'p').text
+					
+					trs = info.find_elements(By.TAG_NAME, 'tr')
 
-	trs = infos[2].find_elements(By.TAG_NAME, 'tr')
-	txt_contents3 = ""
+					for tr in trs:
+						if (tr.get_attribute("class") == "table-row _info" or tr.get_attribute("class") == "table-sub-row _info"):
+							txt_contents3 = txt_contents3 + tr.text + " "
+				except:
+					err = ""
 
-	for tr in trs:
-		if (tr.get_attribute("class") == "table-row _info" or tr.get_attribute("class") == "table-sub-row _info"):
-			txt_contents3 = txt_contents3 + tr.text + " "
+		EAN = driver.find_element(By.XPATH, '//span[contains(@itemprop, "sku")]').text
+		data = {
+			"title" : product_title,
+			"price" : product_price,
+			"Información general" : {
+				"title" : title1,
+				"content" : txt_contents1
+			},
+			"Ingredientes y alérgenos" : {
+				"title" : title2,
+				"content" : txt_contents2
+			},
+			"Nutrientes" : {
+				"title" : title3,
+				"sub title" : sub_title,
+				"content" : txt_contents3
+			},
+			"Conservación y utilización" : {
+				"title" : title4,
+				"content" : txt_contents4
+			},
+			"EAN" : EAN,
+		}
 
-	title4 = infos[4].find_element(By.TAG_NAME, 'h3').text
-	contents4 = infos[4].find_elements(By.TAG_NAME, 'li')
-
-	txt_contents4 = ""
-	for line in contents4:
-		txt_contents4 = txt_contents4 + line.text + " "
-
-	EAN = driver.find_element(By.XPATH, '//span[contains(@itemprop, "sku")]').text
-
-	data = {
-		"title" : product_title,
-		"price" : product_price,
-		"Información general" : {
-			"title" : title1,
-			"content" : txt_contents1
-		},
-		"Ingredientes y alérgenos" : {
-			"title" : title2,
-			"content" : txt_contents2
-		},
-		"Nutrientes" : {
-			"title" : title3,
-			"sub title" : sub_title,
-			"content" : txt_contents3
-		},
-		"Conservación y utilización" : {
-			"title" : title4,
-			"content" : txt_contents4
-		},
-		"EAN" : EAN,
-	}
-
+	except:
+		err = ""
+	
+	driver.close()
+	
 	return json.dumps(data)
 
 app.run()
